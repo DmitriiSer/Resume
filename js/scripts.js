4fc9304e890b75d52e8cce660d5dd6888fa00b16
DmitriiSer <dmitrii.serikov@gmail.com>
2015-07-09 21:47:35 -0400

    Reorganized the whole script to make it easier to read. Added event handler for changeView button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,206 @@
-$(function() {
-    $( document ).tooltip({
-        track: true
-    });
-    var bdgContainer = $(".badge-container"),
-        bdgs = $(".badge"),
-        sections = $("section");
-    var defaultInterval = 1000,
-        defaultAnimDuration = "slow",
-        defaultAnimDelay = 1000;
+// Helper functions
+var Helpers = {
+    // random int belongs [from, to]
+    rnd: function(from, to) { return from + Math.floor(Math.random() * (to + 1)); },
+    // random color
+    rndColor: function() { return "rgb(" + rnd(0, 255) + ", " + rnd(0, 255) + ", " + rnd(0, 255) + ")"; },
+    // random backgroun color
+    rndBgndColor: function(){
+        var backgroundColors = ["#FCFFF5", "#D1DBBD", "#91AA9D", "#3E606F", "#193441"];
+        return backgroundColors[rnd(0,4)];
+    },
+    // transform RGB color to HEX value
+    rgbToHex: function(r,g,b) { return "#" + r.toString(16) + g.toString(16) + b.toString(16); },
+    // fit text into container
+    fitText: function() {
+        var e = $(this);
+        if (e.width() != undefined && e.width() > 0 &&
+            e.height() != undefined && e.height() > 0) {
+            console.log(e[0].innerText);            
+        }
+    },
+    // badge size change
+    setBadgeSize: function(bdg, size, speed) {
+        var minSize = 0;
+        try {
+            if (bdg.css("min-width") != undefined)
+                minSize = parseInt(bdg.css("min-width"));
+            else if (bdg.css("min-height") != undefined)
+                minSize = parseInt(bdg.css("min-height"));
+        } catch(e) {
+            minSize = 0;
+        }
+        if (size < minSize)
+            size = minSize; 
+        if (speed === undefined)
+            speed = "slow";
+        var bdgInner         = bdg.find(".inner"),
+            bdgInnerSize     = size * Math.sin(Math.PI / 4),
+            bdgImg           = bdgInner.find("img"),
+            bdgImgSize       = 0.5 * size,
+            bdgInnerDiv      = bdgInner.find("div"),
+            bdgInnerDivSize  = bdgInnerSize - bdgImgSize,
+            bdgInnerSpan     = bdgInner.find("span");
+        bdg.velocity("stop")
+            .velocity({
+            width: size,
+            height: size }, speed);
+        bdgInner.velocity("stop")
+            .velocity({
+            width: bdgInnerSize,
+            height: bdgInnerSize }, speed);
+        bdgImg.velocity("stop")
+            .velocity({
+            width: bdgImgSize,
+            height: bdgImgSize }, speed);
+        bdgInnerDiv.velocity("stop")
+            .velocity({
+            width: bdgInnerSize,
+            height: bdgInnerDivSize }, {
+            duration: speed,
+            progress: function() {
+                //bdgInnerSpan.position().top = "100px";
+                //bdgInnerSpan.position().top = "100px";
+                //bdgInnerDiv.fitText();
+            }
+        });
+    },
+    // badge in table cell threshold
+    badgeInACellThreshold: function(bdgCount) {
+        var bdgs = $(".badge");
+        var bdgMinSize = parseInt(bdgs.css("min-width")),
+            bdgMarginLeft = parseInt(bdgs.css("marginLeft")),
+            bdgMarginRight = parseInt(bdgs.css("marginRight"));
+        var result = bdgCount * (bdgMinSize + bdgMarginLeft) + bdgMarginRight;
+        console.debug("bdgCount = " + bdgCount + ", bdgMinSize = " + bdgMinSize +
+                      ", bdgMarginLeft = " + bdgMarginLeft +
+                      ", bdgMarginRight = " + bdgMarginRight + 
+                      ", result = " + result);
+        return result;
+    },
+    // responsive badges
+    responsiveBbadges: function() {        
+        var bdg = $(eTd).find(".badge");
+        bdg.each(function(n, eBdg) {
+            console.log(
+                badgeInACellThreshold(4) + " " + $(eTd).width() + " " +
+                badgeInACellThreshold(3) + " " + $(eTd).width()
+            );
+            if ($(eTd).width() < badgeInACellThreshold(6)) {
+                console.log("badgeInACellThreshold(4) = " + badgeInACellThreshold(4) + " => " + $(eTd).width());
+                setBadgeSize($(eBdg), 64);
+            } else if ($(eTd).width() < badgeInACellThreshold(3)) {
+                console.log("badgeInACellThreshold(3) = " + badgeInACellThreshold(4) + " => " + $(eTd).width());
+                setBadgeSize($(eBdg), 32);
+            } else {
+                console.log("no badgeInACellThreshold");
+                setBadgeSize($(eBdg), 64);
+            }
+        });
+    },
+    // responsive table
+    responsiveTable: function(table) {
+        var tds = table.find("td");
+        tds.each(function(n, eTd) {
+            //console.debug("td#" + n + " width=" + $(eTd).width());
+            if ($(eTd).width() < Helpers.badgeInACellThreshold(4)) {
+                //console.log("1");
+            }
+            //responsiveBbadges();
+        });
+    }
+}
+// Event handlers
+var EventHandlers = {
+    // window load event handler
+    loadEventHandler: function() {
+        console.log("window.load()");
+        //Intro animations
+        Animation.introAnimation();
+        EventHandlers.resizeEventHandler();
+    },
+    // window resize event handler
+    resizeEventHandler: function () {
+        //console.log("window.resize()");
+        //console.log("resize: body height = " + $("body").height());
+        var table = $("table");
+        // responsive table
+        Helpers.responsiveTable(table);    
+    },
+    // changeView button click event handler
+    changeViewClickEventHandler: function() {
+        var elems = ["html", "body", "hr"];//, "div", "section"];
+        for(var i = 0; i < elems.length; i++) {
+            if ($(elems[i]).hasClass("print"))
+                $(elems[i]).removeClass("print");
+            else
+                $(elems[i]).addClass("print");
+        }
+        /*if ($("*").hasClass("print"))
+            $("*").removeClass("print");
+        else
+            $("*").addClass("print");*/
+    },
+    // badge mouse enter event handler
+    badgeMouseEnter: function(e) {
+        var background = $(this).find(".background"),
+            img = $(this).find("img"),
+            text = $(this).find("span").parent();
+        //$(this).css("position", "absolute");
+        //$(this).velocity({ left : "90"}, 5000);
+        if (!img.hasClass("velocity-animating")) {
+            //img.css("margin", "0");
+            //img.css("position", "absolute");
+            background.velocity({ blur: 0 }, "fast");
+            img.velocity({ width: "100%", height : "100%", opacity: 0 }, "fast");
+            if (img.length != 0) {
+                text.velocity({ opacity: 0 }, "fast");
+            } else {
+                text.velocity({
+                    width: $(this).width(),
+                    height: $(this).height()
+                }, "fast");
+            }
+        } else {
+            img.velocity("stop");
+            text.velocity("stop");
+        }
+    },
+    badgeMouseLeave: function(e) {
+        //console.log("bdgs.mouseleave");
+        var imgSize = Math.sin(Math.PI / 4) * 100;
+        var background = $(this).find(".background"),
+            img = $(this).find("img"),
+            text = $(this).find("span").parent();
+        background.velocity("stop");
+        img.velocity("stop");
+        text.velocity("stop");
+        background.velocity({ blur: "8px" }, "fast");
+        img.velocity({ width: imgSize + "%", height : imgSize + "%", opacity: 1 }, "fast");
+        if (img.length != 0) {
+            text.velocity({ opacity: 1 }, "fast");
+        } else {
+            text.velocity({
+                width: "100%",
+                height: "100%"
+            }, "fast");
+        }
+    }
+}
+// Animation
+var Animation = {
+    // velocity.js default variables
+    defaultInterval: 1000,
+    defaultAnimDuration: "slow",
+    defaultAnimDelay: 1000,
     //variable that described count of squares in a row
-    var $tCountInARow = 50;
+    tCountInARow: 50,
     //variable indicates triagle size
-    var $tSize = 0;
-    //get a random number
-    function rnd(from, to) { return from + Math.floor(Math.random() * (to + 1)); }
-    //get a random color
-    var backgroundColors = ["#FCFFF5", "#D1DBBD", "#91AA9D", "#3E606F", "#193441"];
-    function rndColor(){ return "rgb(" + rnd(0, 255) + ", " + rnd(0, 255) + ", " + rnd(0, 255) + ")"; }
-    function rndBgndColor(){ return backgroundColors[rnd(0,4)]; }
-    function rgbToHex(r,g,b) { return "#" + r.toString(16) + g.toString(16) + b.toString(16); }
+    tSize: 0,
     //animate a background with triangles
-    var trianglesHaveBeenAnimated = false;
+    trianglesHaveBeenAnimated: false,
     // draw a squares on canvas
-    function drawSquare(obj, p1, colors, alpha) {
+    drawSquare: function(canvas, p1, colors, alpha) {
         // p1----p2
         // |--p5--|
         // p4----p3
@@ -37,7 +215,7 @@ $(function() {
                       rndBgndColor()];
         }
         //get objects drawing context
-        var c2d = $("canvas")[0].getContext('2d');
+        var c2d = canvas.getContext('2d');
         if (c2d === null || c2d === undefined || c2d.length == 0)
             return;
         if (alpha != undefined)
@@ -74,9 +252,9 @@ $(function() {
         c2d.closePath();
         c2d.fillStyle = colors[3];
         c2d.fill();
-    }
+    },
     // animate (popping) a square before drawing it
-    function popSquare(obj, i, j) {
+    popSquare: function(obj, i, j) {
         //animation parameter variables
         var duration = "slow",//2000,
             queue = obj.attr("id"),
@@ -136,14 +314,14 @@ $(function() {
             queue: queue
         });
         obj.dequeue(queue);
-    }
+    },
     // draw field of square on canvas
-    function drawFielOfSquares(obj, rows, columns) {
+    drawFielOfSquares: function(obj, rows, columns) {
         for (var i = 0; i < rows; i++)
             for (var j = 0; j < columns; j++)
                 drawSquare(obj, { x: j * $tSize, y: i * $tSize }, [], 0.2);
-    }
-    function drawPopingSquares(obj, rows, columns) {
+    },
+    drawPopingSquares: function(obj, rows, columns) {
         //var popSquareDivCount = rows * columns / 10;
         var popSquareDivCount = 5;
         for (var id = 0; id < popSquareDivCount; id++) {
@@ -173,8 +351,8 @@ $(function() {
         popSquare(popSquareDiv, 1, 1);
         popSquare(popSquareDiv, 2, 2);
         $("#ps0").dequeue("ps0");*/
-    }
-    function animateBackground() {
+    },
+    animateBackground: function() {
         //create field of triangles on canvas
         $("html").prepend("<div class='canvas'>");
         $("div.canvas").append("<canvas>");
@@ -195,14 +373,14 @@ $(function() {
             drawFielOfSquares($("canvas"), tRows, $tCountInARow);
             drawPopingSquares($("canvas"), tRows, $tCountInARow);
         }
-    }
-    function animateSection(s) {
+    },
+    animateSection: function(s) {
         s.velocity({ opacity: 1 }, {
             duration: defaultAnimDuration,
             complete: function() { animateSection(s.next()); }
         });
-    }
-    function introAnimation() {
+    },
+    introAnimation: function() {
         //animate triangles
         return;
         animateBackground(trianglesHaveBeenAnimated);
@@ -211,6 +389,7 @@ $(function() {
         var int = setInterval(function() {
             if (trianglesHaveBeenAnimated) {
                 //hide sections and set teir entry 'width'
+                var sections = $("section");
                 sections.each(function(n,e) {
                     $(this).css("opacity", 0);
                     //$(this).css("width", $("body").width());
@@ -224,172 +403,18 @@ $(function() {
             }
         }, defaultInterval);
     }
-
-    $(window).load(function() {
-        console.log("window.load()");
-        //Intro animations
-        introAnimation();
-        resize();
-    }); //Window OnLoad event
-    $(window).on("resize", resize); //Window Resize event
-    $("html").on("resize", resize);
-    //Badge mouse enter
-    var imgSize = Math.sin(Math.PI / 4) * 100;
-    bdgs.on("mouseenter", function(e) {
-        //console.log("bdgs.mouseenter");
-        var background = $(this).find(".background"),
-            img = $(this).find("img"),
-            text = $(this).find("span").parent();
-        //$(this).css("position", "absolute");
-        //$(this).velocity({ left : "90"}, 5000);
-        if (!img.hasClass("velocity-animating")) {
-            //img.css("margin", "0");
-            //img.css("position", "absolute");
-            background.velocity({ blur: 0 }, "fast");
-            img.velocity({ width: "100%", height : "100%", opacity: 0 }, "fast");
-            if (img.length != 0) {
-                text.velocity({ opacity: 0 }, "fast");
-            } else {
-                text.velocity({
-                    width: $(this).width(),
-                    height: $(this).height()
-                }, "fast");
-            }
-        } else {
-            img.velocity("stop");
-            text.velocity("stop");
-        }
-    });
-    bdgs.on("mouseleave", function(e) {
-        //console.log("bdgs.mouseleave");
-        var background = $(this).find(".background"),
-            img = $(this).find("img"),
-            text = $(this).find("span").parent();
-        background.velocity("stop");
-        img.velocity("stop");
-        text.velocity("stop");
-        background.velocity({ blur: "8px" }, "fast");
-        img.velocity({ width: imgSize + "%", height : imgSize + "%", opacity: 1 }, "fast");
-        if (img.length != 0) {
-            text.velocity({ opacity: 1 }, "fast");
-        } else {
-            text.velocity({
-                width: "100%",
-                height: "100%"
-            }, "fast");
-        }
-    });
-
-    // Helper: fit text into container size
-    $.fn.fitText = function() {
-        var e = $(this);
-        if (e.width() != undefined && e.width() > 0 &&
-            e.height() != undefined && e.height() > 0) {
-            console.log(e[0].innerText);            
-        }
-    }
-    //Helper: badge size change
-    var setBadgeSize = function(bdg, size, speed) {
-        var minSize = 0;
-        try {
-            if (bdg.css("min-width") != undefined)
-                minSize = parseInt(bdg.css("min-width"));
-            else if (bdg.css("min-height") != undefined)
-                minSize = parseInt(bdg.css("min-height"));
-        } catch(e) {
-            minSize = 0;
-        }
-        if (size < minSize)
-            size = minSize; 
-        if (speed === undefined)
-            speed = "slow";
-        var bdgInner         = bdg.find(".inner"),
-            bdgInnerSize     = size * Math.sin(Math.PI / 4),
-            bdgImg           = bdgInner.find("img"),
-            bdgImgSize       = 0.5 * size,
-            bdgInnerDiv      = bdgInner.find("div"),
-            bdgInnerDivSize  = bdgInnerSize - bdgImgSize,
-            bdgInnerSpan     = bdgInner.find("span");
-        bdg.velocity("stop")
-            .velocity({
-            width: size,
-            height: size }, speed);
-        bdgInner.velocity("stop")
-            .velocity({
-            width: bdgInnerSize,
-            height: bdgInnerSize }, speed);
-        bdgImg.velocity("stop")
-            .velocity({
-            width: bdgImgSize,
-            height: bdgImgSize }, speed);
-        bdgInnerDiv.velocity("stop")
-            .velocity({
-            width: bdgInnerSize,
-            height: bdgInnerDivSize }, {
-            duration: speed,
-            progress: function() {
-                //bdgInnerSpan.position().top = "100px";
-                //bdgInnerSpan.position().top = "100px";
-                //bdgInnerDiv.fitText();
-            }
-        });
-    }
-    // Helper: badge in table cell threshold
-    var badgeInACellThreshold = function(bdgCount) {
-        var bdgMinSize = parseInt(bdgs.css("min-width")),
-            bdgMarginLeft = parseInt(bdgs.css("marginLeft")),
-            bdgMarginRight = parseInt(bdgs.css("marginRight"));
-        console.debug("bdgMinSize = " + bdgMinSize);
-        console.debug("bdgMarginLeft = " + bdgMarginLeft);
-        console.debug("bdgMarginRight = " + bdgMarginRight);
-        return (bdgCount * (bdgMinSize + bdgMarginLeft) + bdgMarginRight);
-    }
-    // Helper: responsive badges
-    var responsiveBbadges = function() {        
-        var bdg = $(eTd).find(".badge");
-        bdg.each(function(n, eBdg) {
-            console.log(
-                badgeInACellThreshold(4) + " " + $(eTd).width() + " " +
-                badgeInACellThreshold(3) + " " + $(eTd).width()
-            );
-            if ($(eTd).width() < badgeInACellThreshold(6)) {
-                console.log("badgeInACellThreshold(4) = " + badgeInACellThreshold(4) + " => " + $(eTd).width());
-                setBadgeSize($(eBdg), 64);
-            } else if ($(eTd).width() < badgeInACellThreshold(3)) {
-                console.log("badgeInACellThreshold(3) = " + badgeInACellThreshold(4) + " => " + $(eTd).width());
-                setBadgeSize($(eBdg), 32);
-            } else {
-                console.log("no badgeInACellThreshold");
-                setBadgeSize($(eBdg), 64);
-            }
-        });
-    }
-    // Helper: responsive table
-    var responsiveTable = function(table) {
-        var tds = table.find("td");
-        console.debug(badgeInACellThreshold(1));
-        tds.each(function(n, eTd) {
-            console.debug("td#" + n + " width=" + $(eTd).width());
-            if ($(eTd).width() < badgeInACellThreshold(4)) {
-                console.log("1");
-            }
-            //responsiveBbadges();
-        });
-    }
-    //Window Resize event handler
-    function resize(){
-        //console.log("window.resize()");
-        //Badge font size calculation
-        //bdgs.width("10px");
-        //bdgs.height(bdgs.width());
-        //bdgContainer
-        //bdgs.css("margin", "30px");
-        //console.log("resize: body height = " + $("body").height());
-        var bdgMinSize = parseInt(bdgs.css("min-width")),
-            bdgMarginLeft = parseInt(bdgs.css("marginLeft")),
-            bdgMarginRight = parseInt(bdgs.css("marginRight"));    
-        var table = $("table");
-        // responsive table
-        responsiveTable(table);
-    }
+}
+// jQuery ready function
+$(function() {
+    // jQuery-UI tooltips activation
+    $(document).tooltip({ track: true });
+    // Attaching events
+    $(window).load(EventHandlers.loadEventHandler);             // Window OnLoad event
+    $(window).on("resize", EventHandlers.resizeEventHandler);   // Window resize event
+    $("html").on("resize", EventHandlers.resizeEventHandler);   // HTML resize event
+    $("#changeView").on("click", EventHandlers.changeViewClickEventHandler); // changeView button click event handler
+    $(".badge").on("mouseenter", EventHandlers.badgeMouseEnter);// Badge mouse enter event
+    $(".badge").on("mouseleave", EventHandlers.badgeMouseLeave);// Badge mouse leave event
+    // click changeView button
+    //$("#changeView").click();
 });
