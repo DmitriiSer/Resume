91900e681534c268ccf2e3c13ce4902710b3d264
DmitriiSer <dmitrii.serikov@gmail.com>
2020-12-19 16:19:24 -0500

    Added toolbar.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    // Material
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  declarations: [HomeComponent],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
