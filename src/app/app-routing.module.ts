d3e6a25fddbfb55a8f1a1deddb683f4fcaabdd58
DmitriiSer <dmitrii.serikov@gmail.com>
2020-12-21 13:23:49 -0500

    Added projects route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './modules/about/about.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
